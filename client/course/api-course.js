8a4465b01e22d54986b2edf296c0781169694ae0
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-08-04 15:40:56 -0700

    fix for crud operations

--- client/course/api-course.js
+++ client/course/api-course.js
@@ -12,25 +12,21 @@ const create = async (params, credentials, course) => {
         } catch(err) { 
           console.log(err)
         }
-}
-
-const listByInstructor = async (params, credentials, signal) => {
+  }
+  
+  const list = async (signal) => {
     try {
-      let response = await fetch('/api/courses/by/'+params.userId, {
+      let response = await fetch('/api/courses/', {
         method: 'GET',
         signal: signal,
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Bearer ' + credentials.t
-        }
       })
-      return response.json()
+      return await response.json()
     } catch(err) {
       console.log(err)
     }
-}
-
-const read = async (params, signal) => {
+  }
+  
+  const read = async (params, signal) => {
     try {
       let response = await fetch('/api/courses/' + params.courseId, {
         method: 'GET',
@@ -78,10 +74,60 @@ const read = async (params, signal) => {
     }
   }
 
-export { 
-    create, 
-    listByInstructor,
+  const listByInstructor = async (params, credentials, signal) => {
+    try {
+      let response = await fetch('/api/courses/by/'+params.userId, {
+        method: 'GET',
+        signal: signal,
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        }
+      })
+      return response.json()
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+  const newLesson = async (params, credentials, lesson) => {
+    try {
+      let response = await fetch('/api/courses/'+params.courseId+'/lesson/new', {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        },
+        body: JSON.stringify({lesson:lesson})
+      })
+      return response.json()
+    } catch(err){
+      console.log(err)
+    }
+  }
+  const listPublished = async (signal) => {
+    try {
+      let response = await fetch('/api/courses/published', {
+        method: 'GET',
+        signal: signal,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      })
+      return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  export {
+    create,
+    list,
     read,
     update,
     remove,
-}
+    listByInstructor,
+    newLesson,
+    listPublished
+  }
